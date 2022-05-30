74e5071a740c8be97fb491b3bf76ac98ec57690a
sp-ino <spinovale97@gmail.com>
2022-05-30 13:43:21 +0200

    Start experimenting with the aes block

--- sources_1/new/aes_ip.vhd
+++ sources_1/new/aes_ip.vhd
@@ -32,6 +32,8 @@ use IEEE.std_logic_1164.all;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
+
+
 entity aes_ip is
     Port ( textin : in std_logic_vector (127 downto 0);
            rst : in std_logic;
@@ -39,13 +41,47 @@ entity aes_ip is
            textout : out std_logic_vector (127 downto 0));
 end aes_ip;
 
+
 architecture behavioral of aes_ip is
 
     constant n_bytes: integer := 16;
     constant byte_len: integer := 8;
     type aes_port is array (n_byte - 1 downto 0) of std_logic_vector (byte_len - 1 downto 0);
 
+    constant key: aes_port := (others => (others => '1'));
+    signal in_bytes: aes_port;
+    signal out_bytes: aes_port;
+
 begin
 
+    input_conversion: process (textin)
+    begin
+        for idx in n_bytes - 1 downto 0
+        loop
+            in_bytes(idx) <= textin(byte_len*idx - 1 downto byte_len*(idx - 1));
+        end loop;
+    end process input_conversion;
+
+
+    xor_key: process (ck, rst)
+    begin
+        if rst = '1' then
+            out_bytes <= (others => (others => '0'));
+        elsif rising_edge(ck) then
+            for idx in n_bytes - 1 downto 0
+            loop
+                out_bytes(idx) <= in_bytes(idx) ^ key(idx);
+            end loop;
+        end if;  
+    end process xor_key;
+
+
+    output_conversion: process (out_bytes)
+    begin
+        for idx in n_bytes - 1 downto 0
+        loop
+            textout(byte_len*idx - 1 downto byte_len*(idx - 1)) <= out_bytes(idx);
+        end loop;
+    end process output_conversion;
 
 end behavioral;
