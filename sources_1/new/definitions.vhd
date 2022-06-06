faeeec65f8dc74a511afd5549f72675606678365
sp-ino <spinovale97@gmail.com>
2022-06-06 20:29:24 +0200

    Fix implementation of rotr and rotl functions

--- sources_1/new/definitions.vhd
+++ sources_1/new/definitions.vhd
@@ -60,20 +60,22 @@ package body definitions is
     function rotl(byte: std_logic_vector (byte_len - 1 downto 0); amount: integer) return std_logic_vector is
     -- Performs an unsigned left rotation on an 8-bit std_logic_vector
         variable rotated: std_logic_vector (byte_len - 1 downto 0);
+        variable temp: std_logic;
 
     begin
 
         -- Is there a (synthesizable) way to check that byte_len - amount does not become < 0 ? Is it recommended/advisable to check it?
         rotated := byte;
         for idx in amount - 1 downto 0 loop
+            temp := rotated(0);
             rotated(0) := rotated(7);
-            rotated(1) := rotated(0);
-            rotated(2) := rotated(1);
-            rotated(3) := rotated(2);
-            rotated(4) := rotated(3);
-            rotated(5) := rotated(4);
-            rotated(6) := rotated(5);
             rotated(7) := rotated(6);
+            rotated(6) := rotated(5);
+            rotated(5) := rotated(4);
+            rotated(4) := rotated(3);
+            rotated(3) := rotated(2);
+            rotated(2) := rotated(1);
+            rotated(1) := temp;
         end loop;
         return rotated;
 
@@ -83,6 +85,7 @@ package body definitions is
     function rotr(byte: std_logic_vector (byte_len - 1 downto 0); amount: integer) return std_logic_vector is
     -- Performs an unsigned right rotation on an 8-bit std_logic_vector
         variable rotated: std_logic_vector (byte_len - 1 downto 0);
+        variable temp: std_logic;
 
     begin
 
@@ -90,6 +93,7 @@ package body definitions is
         -- rotated := std_logic_vector(shif_right(unsigned(byte), amount)) xor std_logic_vector(shif_right(unsigned(byte), byte_len - amount));
         rotated := byte;
         for idx in amount - 1 downto 0 loop
+            temp := rotated(7);
             rotated(7) := rotated(0);
             rotated(0) := rotated(1);
             rotated(1) := rotated(2);
@@ -97,7 +101,7 @@ package body definitions is
             rotated(3) := rotated(4);
             rotated(4) := rotated(5);
             rotated(5) := rotated(6);
-            rotated(6) := rotated(7);
+            rotated(6) := temp;
         end loop;
         return rotated;
 
